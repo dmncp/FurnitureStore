f709d60e10c8afc7cf6b561ff57f87926e9a2d19
dmncp <damiancyper@gmail.com>
2021-07-13 17:36:14 +0200

    create models

---
+++ furniture_store/store/admin.py
@@ -0,0 +1,46 @@
+from django.contrib import admin
+from .models import *
+
+
+# Register your models here.
+@admin.register(FurnitureType)
+class FurnitureTypeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_filter = ['name']
+
+
+@admin.register(Furniture)
+class FurnitureAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'type', 'price', 'discount', 'amount', 'state']
+    list_filter = ['name', 'type', 'price', 'state']
+
+
+@admin.register(Opinion)
+class OpinionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'furniture', 'user', 'rating']
+    list_filter = ['furniture', 'user', 'rating']
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'furniture']
+    list_filter = ['user', 'furniture']
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['id', 'city', 'street', 'house_nr', 'building_nr', 'zip_code']
+    list_filter = ['city', 'street', 'house_nr', 'zip_code', 'building_nr']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'order_nr', 'price', 'status']
+    list_filter = ['user', 'price', 'order_nr', 'status']
+
+
+@admin.register(UserAddress)
+class UserAddress(admin.ModelAdmin):
+    list_display = ['id', 'user', 'address']
+    list_filter = ['user', 'address']
+
---
+++ furniture_store/store/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+STATE = (
+    ('new', 'Nowy'),
+    ('used', 'Używany')
+)
+
+ORDER_STATUS = (
+    ('unsent', 'Zamówienie nie zostało jeszcze wysłane'),
+    ('sent', 'Zamówienie zostało wysłane. Jest już w drodze'),
+    ('ordered', 'Zamówienie zostało dostarczone')
+)
+
+
+# Create your models here.
+class FurnitureType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Furniture(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.ForeignKey(FurnitureType, on_delete=models.CASCADE)
+    state = models.CharField(choices=STATE, default='new', max_length=20)
+    price = models.IntegerField(default=200)
+    origin = models.CharField(max_length=255)
+    material = models.CharField(max_length=255)
+    amount = models.IntegerField(default=0)
+    description = models.TextField()
+    warranty_time = models.IntegerField(default=0)
+    discount = models.IntegerField(default=0)
+    height = models.IntegerField()
+    width = models.IntegerField()
+    depth = models.IntegerField()
+
+
+class Opinion(models.Model):
+    furniture = models.ForeignKey(Furniture, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
+    rating = models.IntegerField()
+    opinion = models.TextField()
+
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
+    furniture = models.ForeignKey(Furniture, on_delete=models.CASCADE)
+
+
+class Address(models.Model):
+    city = models.CharField(max_length=255)
+    street = models.CharField(max_length=255)
+    house_nr = models.CharField(max_length=5)
+    building_nr = models.CharField(max_length=5)
+    zip_code = models.CharField(max_length=7)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
+    order_nr = models.CharField(max_length=7)
+    price = models.IntegerField()
+    products = models.ManyToManyField(Furniture, symmetrical=False, related_name='ordered_products')
+    status = models.CharField(choices=ORDER_STATUS, default='unsent', max_length=20)
+
+
+class UserAddress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, default=0)
