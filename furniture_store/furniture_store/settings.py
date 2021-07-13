541b435e5617fa87ebccbf61562ec6af6af37d78
dmncp <damiancyper@gmail.com>
2021-07-13 18:19:00 +0200

    reset password system, but it doesn't work ...

--- furniture_store/furniture_store/settings.py
+++ furniture_store/furniture_store/settings.py
@@ -128,3 +128,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
