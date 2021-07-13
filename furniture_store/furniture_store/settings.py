13fa3eb0916a6e76891f8cd293c15767790fac9b
dmncp <damiancyper@gmail.com>
2021-07-13 17:56:34 +0200

    login/logout system

--- furniture_store/furniture_store/settings.py
+++ furniture_store/furniture_store/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'furniture_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            str(BASE_DIR.joinpath('templates'))
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +126,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
