18c507982c904b92f7e52db7a729b2c3aa24ae57
dmncp <damiancyper@gmail.com>
2021-07-16 19:39:01 +0200

    home view done

--- furniture_store/store/urls.py
+++ furniture_store/store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from django.views.generic.base import TemplateView
 from .views import SignUpView
 
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('accounts/signup/', SignUpView.as_view(), name='signup'),
 ]
