3f78db91e45726e9685e8d01e8e0fc197645bdc6
dmncp <damiancyper@gmail.com>
2021-07-28 17:25:42 +0200

    admin settings and sorting options

--- furniture_store/store/urls.py
+++ furniture_store/store/urls.py
@@ -13,9 +13,9 @@ urlpatterns = [
     path('settings/address/edit/<int:pk>', AddressEditFormView.as_view(template_name='address_edit_form.html'), name='address_form'),
     path('settings/address/delete/<int:pk>', delete_address, name='address_delete'),
     path('settings/customer_orders', CustomerOrdersView.as_view(template_name='customer_orders.html'), name='customer_orders'),
-    path('settings/customer_orders/details/<int:pk>', OrderDetailsView.as_view(template_name='order_details.html'), name='order_details'),
+    path('settings/customer_orders/details/<str:pk>', OrderDetailsView.as_view(template_name='order_details.html'), name='order_details'),
     path('settings/opinions', OpinionsView.as_view(template_name='opinions.html'), name='opinions'),
-    path('settings/opinions/delete/<int:pk>', delete_opinion, name='delete_opinion'),
+    path('settings/opinions/delete/<int:pk>', delete_opinion_user, name='delete_opinion'),
     path('settings/opinions/edit/<int:pk>', OpinionEditFormView.as_view(template_name='opinions_edit.html'), name='opinion_edit'),
     path('settings/delete_account', delete_account, name='delete_user'),
     path('settings/delete_confirm', TemplateView.as_view(template_name='delete_user.html'), name='delete_user_confirmation'),
@@ -26,5 +26,14 @@ urlpatterns = [
     path('settings/order_archive/edit/<str:pk>', staff_member_required(EditOrderView.as_view(template_name='order_edit.html')), name='order_edit'),
     path('settings/order_archive/edit/delete/<str:order_nr>/<int:prod_nr>', staff_member_required(delete_product_from_order), name='prod_delete'),
     path('settings/order_archive/edit/add/<str:pk>', staff_member_required(AddProductToOrder.as_view(template_name='order_edit_add.html')), name='order_edit_add'),
-    path('settings/order_archive/edit/add/<str:order_nr>/<int:prod_nr>', staff_member_required(add_product), name='add_to_order')
+    path('settings/order_archive/edit/add/<str:order_nr>/<str:prod_nr>', staff_member_required(add_product), name='add_to_order'),
+    path('settings/opinions_all', staff_member_required(OpinionsAdminView.as_view(template_name='opinions_edit_admin.html')), name='opinions_admin'),
+    path('settings/opinions_all/edit/<str:pk>', staff_member_required(delete_opinion_description), name='opinions_admin_edit'),
+    path('settings/opinions_all/delete/<str:pk>', staff_member_required(delete_opinion_admin), name='opinions_admin_delete'),
+    path('settings/all_products', staff_member_required(AllProductsView.as_view(template_name='products_all.html')), name='all_products'),
+    path('settings/all_products/edit/<int:pk>', staff_member_required(EditProductView.as_view(template_name='admin_product_edit.html')), name='edit_product_admin'),
+    path('settings/all_products/delete_category/<int:pk>', staff_member_required(delete_category), name='delete_category'),
+    path('settings/all_products/edit_category/<int:pk>', staff_member_required(EditCategoryView.as_view(template_name='category_edit.html')), name='category_edit'),
+    path('settings/users', staff_member_required(UserListView.as_view(template_name='users.html')), name='users'),
+    path('settings/users/delete/<int:pk>', staff_member_required(delete_account_admin), name='delete_user')
 ]
--- furniture_store/store/views.py
+++ furniture_store/store/views.py
@@ -1,14 +1,20 @@
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views import generic, View
+from django.contrib import messages
 from .forms import UserCreationForm, ContactForm, UserChangeForm
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from .models import *
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+
+
+class SuperUserCheck(UserPassesTestMixin, View):
+    def test_func(self):
+        return self.request.user.is_superuser
 
 
 class SignUpView(generic.CreateView):
@@ -117,6 +123,18 @@ class CustomerOrdersView(LoginRequiredMixin, generic.ListView):
         orders = super().get_queryset()
         orders = orders.filter(user=self.request.user)
 
+        order_nr = self.request.GET.get('order_nr', None)
+        sort_by = self.request.GET.get('sort_by', None)
+        order = self.request.GET.get('order', None)
+
+        if order_nr:
+            orders = orders.filter(order_nr=order_nr)
+
+        if sort_by and order:
+            if order == 'desc':
+                sort_by = '-' + sort_by
+            orders = orders.order_by(sort_by)
+
         return orders
 
 
@@ -154,15 +172,29 @@ class OpinionsView(LoginRequiredMixin, generic.ListView):
         opinions = super().get_queryset()
         opinions = opinions.filter(user=self.request.user)
 
+        prod_name = self.request.GET.get('prod_name', None)
+        sort_by = self.request.GET.get('sort_by', None)
+        order = self.request.GET.get('order', None)
+
+        if prod_name:
+            opinions = opinions.filter(furniture__name=prod_name)
+
+        if sort_by and order:
+            if order == 'desc':
+                sort_by = '-' + sort_by
+            opinions = opinions.order_by(sort_by)
+
         return opinions
 
 
-@login_required
-def delete_opinion(request, pk):
+def delete_opinion(pk):
     opinion = Opinion.objects.get(id=pk)
-
     opinion.delete()
 
+
+@login_required
+def delete_opinion_user(request, pk):
+    delete_opinion(pk)
     return redirect('opinions')
 
 
@@ -190,36 +222,38 @@ class OpinionEditFormView(LoginRequiredMixin, generic.ListView):
         return context
 
 
+def delete_user_data(request, pk):
+    user = User.objects.get(id=pk)
+    # delete addresses
+    addresses = UserAddress.objects.filter(user=user)
+    for address in addresses:
+        address.delete()
+
+    # delete shopping carts
+    shopping_cart = ShoppingCart.objects.filter(user=user)
+    for cart in shopping_cart:
+        cart.delete()
+
+    # delete orders
+    orders = Order.objects.filter(user=user)
+    for order in orders:
+        order.delete()
+
+    # delete opinions
+    opinions = Opinion.objects.filter(user=user)
+    for opinion in opinions:
+        opinion.delete()
+
+
 @login_required
 def delete_account(request):
-    if not request.user.is_staff:
-        # delete addresses
-        addresses = UserAddress.objects.filter(user=request.user)
-        for address in addresses:
-            address.delete()
+    delete_user_data(request, request.user.id)
 
-        # delete shopping carts
-        shopping_cart = ShoppingCart.objects.filter(user=request.user)
-        for cart in shopping_cart:
-            cart.delete()
-
-        # delete orders
-        orders = Order.objects.filter(user=request.user)
-        for order in orders:
-            order.delete()
-
-        # delete opinions
-        opinions = Opinion.objects.filter(user=request.user)
-        for opinion in opinions:
-            opinion.delete()
-
-        # logout user
-        user_id = request.user.id
-        logout(request)
-
-        # delete account
-        user = User.objects.get(id=user_id)
-        user.delete()
+    # logout user
+    logout(request)
+    # delete account
+    user = User.objects.get(id=request.user.id)
+    user.delete()
 
     return redirect('home')
 
@@ -232,11 +266,22 @@ class UnavailableProductsView(LoginRequiredMixin, generic.ListView):
         furniture = furniture.filter(amount=0)
         prod_id = self.request.GET.get('product_id', None)
         name = self.request.GET.get('name', None)
+        category_name = self.request.GET.get('category_name', None)
+
+        sort_by = self.request.GET.get('sort_by', None)
+        order = self.request.GET.get('order', None)
 
         if prod_id:
             furniture = furniture.filter(id=prod_id)
         if name:
             furniture = furniture.filter(name=name)
+        if category_name:
+            furniture = furniture.filter(type__name=category_name)
+
+        if sort_by and order:
+            if order == 'desc':
+                sort_by = '-' + sort_by
+            furniture = furniture.order_by(sort_by)
 
         return furniture
 
@@ -248,11 +293,18 @@ class AllOrdersView(LoginRequiredMixin, generic.ListView):
         order = super().get_queryset()
         order_nr = self.request.GET.get('order_nr', None)
         client_id = self.request.GET.get('client_id', None)
+        sort_by = self.request.GET.get('sort_by', None)
+        sort_order = self.request.GET.get('order', None)
 
         if order_nr:
             order = order.filter(order_nr=order_nr)
         if client_id:
             order = order.filter(user__id=client_id)
+
+        if sort_by and sort_order:
+            if sort_order == 'desc':
+                sort_by = '-' + sort_by
+            order = order.order_by(sort_by)
 
         return order
 
@@ -335,3 +387,231 @@ def add_product(request, order_nr, prod_nr):
         order.products.add(order_product)
 
     return redirect('order_edit_add', order_nr)
+
+
+class OpinionsAdminView(LoginRequiredMixin, generic.ListView):
+    model = Opinion
+
+    def get_queryset(self):
+        opinions = super().get_queryset()
+        opinion_id = self.request.GET.get('opinion_id', None)
+        client_id = self.request.GET.get('client_id', None)
+        product_name = self.request.GET.get('product_name', None)
+
+        if opinion_id:
+            opinions = opinions.filter(id=opinion_id)
+        if client_id:
+            opinions = opinions.filter(user__id=client_id)
+        if product_name:
+            opinions = opinions.filter(furniture__name=product_name)
+
+        return opinions
+
+
+@login_required
+def delete_opinion_admin(request, pk):
+    delete_opinion(pk)
+    return redirect('opinions_admin')
+
+
+@login_required
+def delete_opinion_description(request, pk):
+    opinion = Opinion.objects.get(id=pk)
+
+    opinion.opinion = ''
+    opinion.save()
+
+    return redirect('opinions_admin')
+
+
+class AllProductsView(LoginRequiredMixin, generic.ListView):
+    model = Furniture
+
+    def get_queryset(self):
+        products = super().get_queryset()
+        prod_id = self.request.GET.get('prod_id', None)
+        price_from = self.request.GET.get('price_from', None)
+        price_to = self.request.GET.get('price_to', None)
+        category = self.request.GET.get('category', None)
+        prod_name = self.request.GET.get('prod_name', None)
+        state = self.request.GET.get('state', None)
+
+        if prod_id:
+            products = products.filter(id=prod_id)
+        if price_from:
+            products = products.filter(price__gte=price_from)
+        if price_to:
+            products = products.filter(price__lte=price_to)
+        if category:
+            products = products.filter(type__name=category)
+        if prod_name:
+            products = products.filter(name=prod_name)
+        if state:
+            products = products.filter(state=state)
+
+        return products
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = FurnitureType.objects.all()
+
+        return context
+
+
+class EditProductView(LoginRequiredMixin, generic.ListView):
+    model = Furniture
+
+    def post(self, *args, **kwargs):
+        prod_name = self.request.POST['prod_name']
+        prod_type_name = self.request.POST['prod_type']
+        prod_state = self.request.POST['prod_state']
+        prod_price = self.request.POST['prod_price']
+        prod_origin = self.request.POST['prod_origin']
+        prod_material = self.request.POST['prod_material']
+        prod_desc = self.request.POST['prod_desc']
+        prod_amount = self.request.POST['prod_amount']
+        prod_warranty = self.request.POST['prod_warranty']
+        prod_discount = self.request.POST['prod_discount']
+        prod_height = self.request.POST['prod_height']
+        prod_width = self.request.POST['prod_width']
+        prod_depth = self.request.POST['prod_depth']
+        prod_type = FurnitureType.objects.get(name=prod_type_name)
+
+        if self.kwargs['pk'] == 0:
+            prod_to_edit = Furniture(name=prod_name, type=prod_type, state=prod_state, price=prod_price,
+                                     origin=prod_origin, material=prod_material, description=prod_desc,
+                                     amount=prod_amount, warranty_time=prod_warranty, discount=prod_discount,
+                                     height=prod_height, width=prod_width, depth=prod_depth)
+        else:
+            prod_to_edit = Furniture.objects.get(id=self.kwargs['pk'])
+            if prod_name and prod_type_name and prod_state and prod_price and prod_origin and prod_material and prod_desc \
+                    and prod_amount and prod_warranty and prod_discount and prod_height and prod_depth and prod_width:
+                prod_to_edit.name = prod_name
+                prod_to_edit.state = prod_state
+                prod_to_edit.price = prod_price
+                prod_to_edit.origin = prod_origin
+                prod_to_edit.material = prod_material
+                prod_to_edit.description = prod_desc
+                prod_to_edit.amount = prod_amount
+                prod_to_edit.warranty_time = prod_warranty
+                prod_to_edit.discount = prod_discount
+                prod_to_edit.height = prod_height
+                prod_to_edit.width = prod_width
+                prod_to_edit.depth = prod_depth
+                prod_to_edit.type = prod_type
+
+        prod_to_edit.save()
+        return redirect('all_products')
+
+    def get_queryset(self):
+        products = super().get_queryset()
+        product = products.filter(id=self.kwargs['pk'])
+        return product
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = FurnitureType.objects.all()
+        context['pk'] = self.kwargs['pk']
+
+        return context
+
+
+@login_required
+def delete_category(request, pk):
+    category = FurnitureType.objects.get(id=pk)
+    products = Furniture.objects.filter(type=category)
+
+    if products.exists():
+        messages.warning(request, f'Kategoria {category.name} jest w użyciu. Operacja usunięcia niemożliwa.')
+    else:
+        category.delete()
+
+    return redirect('all_products')
+
+
+class EditCategoryView(SuperUserCheck, generic.ListView):
+    model = FurnitureType
+
+    def post(self, *args, **kwargs):
+        cat_name = self.request.POST['cat_name']
+        if self.kwargs['pk'] == 0:
+            category = FurnitureType(name=cat_name)
+        else:
+            category = FurnitureType.objects.get(id=self.kwargs['pk'])
+            if cat_name:
+                category.name = cat_name
+
+        category.save()
+        return redirect('all_products')
+
+    def get_queryset(self):
+        categories = super().get_queryset()
+        category = categories.filter(id=self.kwargs['pk'])
+        return category
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pk'] = self.kwargs['pk']
+
+        return context
+
+
+class UserListView(SuperUserCheck, generic.ListView):
+    model = User
+
+    def post(self, *args, **kwargs):
+        permission = self.request.POST['permission_new']
+        permission = permission.split('+')
+        user = User.objects.get(id=permission[1])
+
+        if permission[0] == 'client':
+            user.is_staff = False
+            user.is_superuser = False
+        elif permission[0] == 'worker':
+            user.is_staff = True
+            user.is_superuser = False
+        elif permission[0] == 'admin':
+            user.is_staff = True
+            user.is_superuser = True
+        user.save()
+
+        return redirect('users')
+
+    def get_queryset(self):
+        users = super().get_queryset()
+        user_id = self.request.GET.get('user_id', None)
+        user_name = self.request.GET.get('user_name', None)
+        permission = self.request.GET.get('permission', None)
+
+        sort_by = self.request.GET.get('sort_by', None)
+        sort_order = self.request.GET.get('order', None)
+
+        if user_id:
+            users = users.filter(id=user_id)
+        if user_name:
+            users = users.filter(username=user_name)
+        if permission:
+            if permission == 'client':
+                users = users.filter(is_staff=False)
+            elif permission == 'worker':
+                users = users.filter(is_staff=True, is_superuser=False)
+            else:
+                users = users.filter(is_superuser=True)
+
+        if sort_by and sort_order:
+            if sort_order == 'desc':
+                sort_by = '-' + sort_by
+            users = users.order_by(sort_by)
+
+        return users
+
+
+@login_required
+def delete_account_admin(request, pk):
+    delete_user_data(request, pk)
+
+    # delete account
+    user = User.objects.get(id=pk)
+    user.delete()
+
+    return redirect('users')
